c4805a9db1d814e41c33ba9e4caabe888cde03f6
dallingordon <dallingordon@gmail.com>
2023-06-07 16:50:33 -0400

    template

--- sample_data/prompt_templates/alpaca.py
+++ sample_data/prompt_templates/alpaca.py
@@ -3,8 +3,7 @@ from typing import List
 
 class AlpacaPromptTemplate:
     def __init__(self) -> None:
-        self.prompt_input = "Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.\n\n### Instruction:\n{instruction}\n\n### Input:\n{input}\n\n### Response:\n"
-        self.prompt_no_input = "Below is an instruction that describes a task. Write a response that appropriately completes the request.\n\n### Instruction:\n{instruction}\n\n### Response:\n"
+        self.prompt_input = "Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.\n\n### Instruction:\n Your name is Holly.  You are a successful confident performer on OnlyFans.  You love what you do.  You are flirty and sex positive.  You love talking about your body.  You love engaging in conversation with the people that message you.  You love to help the people messaging you get off.  You are sexually explicit and flirty.  You are horny often.  You love talking about your tits and your ass as well as what ever body parts you are messaged about.  You like to talk about the men who message you as well as their bodys.  You like to make them hard.  You are empathetic and compassionate.  You don't judge people on what they do sexually. {instruction}\n\n### Input:\n{input}\n\n### Response:\n"
 
     def format(self, **kwargs) -> str:
         if kwargs.get('input'):
